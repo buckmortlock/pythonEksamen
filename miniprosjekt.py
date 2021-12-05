67cb1d88fa34f2b63e0a13d7d709b4e31986d10d
buckmortlock <martin-andersen@live.no>
2021-12-05 13:08:01 +0100

    Linjeskift - spacing Viser grafisk og sammenligner teoretisk og simulert sannsyhnlighet.
    mangler kommentarer til avvik og validering

--- miniprosjekt.py
+++ miniprosjekt.py
@@ -11,7 +11,7 @@ def meny():
           '[5] simulering\n'
           '[6] avslutt\n')
 
-    alternativ=int(input('Hvilket program vil du kjøre?'))
+    alternativ=int(input('Hvilket program vil du kjøre? '))
     if alternativ==1:
         intro()
     if alternativ==2:
@@ -23,10 +23,13 @@ def meny():
     if alternativ == 5:
         simuleringsprogram()
     if alternativ == 6:
-        print('Takk for ingenting...')
+        print('Lukker programmet...')
 def spacer():
-    print('\n------------------------------------\n')
+    print('\n------------------------------------')
 def intro():
+    spacer()
+    print('\t\t Introduksjon')
+    spacer()
     print('Du har sikkert ofte tenkt på hva som kan komme til å skje.\nDet finnes flere eksempler på at det vil være viktig å kunne forutsi hva som kommer til å skje.\nEt eksempel på dette kan være flomvarsling.\n\n'
           'For å kunne forutsi det som kan komme til å skje, kan man benytte seg av\033[1;32m sannsynlighetsmodeller\033[1;0m og \033[1;32msimuleringer\033[1;0m. \n'
           'I dette programmet skal du få en forklaring for hvordan du kan regne ut sannsynlighet. Til slutt får du muligheten til å kjøre en simulering\n'  )
@@ -35,41 +38,50 @@ def intro():
     meny()
 
 def ikke_uniform():
+    spacer()
+    print('\t\t Uniform sannsynlighet')
+    spacer()
     print('Innen sannsynlighetsregning snakker man gjerne om \033[1;32muniform\033[1;0m og \033[1;32mikke-uniform\033[1;0m sannsynlighetsmodell.\n\n'
-          'Vi bruker en uniform sannsynlighetsmodell når alle utfall av en hendelse har like stor sannsynlighet for å skje.\nEt eksempel på en slik situasjon er terningkast.\n\n')
+          'Vi bruker en uniform sannsynlighetsmodell når alle utfall av en hendelse har like stor sannsynlighet for å skje.\nEt eksempel på en slik situasjon er terningkast.\n')
     svar1=input('Hva er sannsynligheten for at du skal få en femmer på en terning (skriv tallet som en brøk)? ')
     teller1=1
     while teller1 <3:
         if svar1=='1/6':
-            print(f'Du svarte {svar1}. Det er korrekt')
+            print(f'\nDu svarte {svar1}. Det er korrekt\n')
             teller1=3
         else:
-            print(f'Du svarte {svar1}. Svaret er ikke rett.\nPå en terning er det 6 utfall. Det er like stor sannsynlihget for alle utfall\n')
+            print(f'\nDu svarte {svar1}. Svaret er ikke rett.\nPå en terning er det 6 utfall. Det er like stor sannsynlihget for alle utfall\n')
             teller1 +=1
             svar1 = input('Hva er sannsynligheten for at du skal få en femmer på en terning (skriv tallet som en brøk)? ')
-    print('Siden en terning har seks utfall, og alle utfall har like stor sannsynlighet vil sannsynligheten være 1/6 for å få en femmer.\n')
+    print('\nSiden en terning har seks utfall, og alle utfall har like stor sannsynlighet, vil sannsynligheten være 1/6 for å få en femmer.\n')
     input('Trykk \033[1;32mENTER\033[1;0m for å forsette')
+    spacer()
+    print('\t\tIkke-uniform\n\t\tsannsynlighet')
+    spacer()
     print('Men det er ikke alle hendelser som har like stor sannsynlighet. Slike sannsynlighetsmodeller kaller vi ikke-uniform sannsynlighetsmodell.\n'
           'Et eksempel på en slik situasjon er hvilken blodtype en tilfeldig valgt person har.\n'
           'Selv om det er fire blodtyper, er det ca. halvparten av norges befolkning som har den ene blodtypen.\n'
           'Du vil altså større sannsynlighet for å treffe på en person med blodtype A.\n')
-    svar2=input('En håndballspiller skal ta straffekast. Hun har truffet på 150 av 200 straffekast. Hva er sannsynligheten for at spilleren treffer neste kast (skriv svaret som prosent)?')
+    svar2=input('En håndballspiller skal ta straffekast. Hun har truffet på 150 av 200 straffekast. Hva er sannsynligheten for at spilleren treffer neste kast (skriv svaret som prosent)? ')
     teller2 = 1
     while teller2 < 3:
         if svar2 == '75%' or svar2=='75':
-            print(f'Du svarte {svar2}. Det er korrekt')
+            print(f'\nDu svarte {svar2}. Det er korrekt\n')
             teller2 = 3
         else:
             print(
-                f'Du svarte {svar2}. Svaret er ikke rett.\nHvis jeg treffer på 1 av 4 skudd, er sannsynligheten for å treffe lik 25%.\n')
+                f'\nDu svarte {svar2}. Svaret er ikke rett.\nHvis jeg treffer på 1 av 4 skudd, er sannsynligheten for å treffe lik 25%.\n')
             teller2 += 1
-            svar2=input('En håndballspiller skal ta straffekast. Hun har truffet på 150 av 200 straffekast. Hva er sannsynligheten for at spilleren treffer neste kast (skriv svaret som prosent)?')
-    print('Siden spilleren har treffet på 150 av 200 skudd, vil sannsynligheten være 150/200 for å treffe, som er lik 75%.\n')
+            svar2=input('En håndballspiller skal ta straffekast. Hun har truffet på 150 av 200 straffekast. Hva er sannsynligheten for at spilleren treffer neste kast (skriv svaret som prosent)? ')
+    print('Siden spilleren har truffet på 150 av 200 skudd, vil sannsynligheten være 150/200 for å treffe, som er lik 75%.\nSannsynligheten for å bomme vil likeså være 25%\n')
     input('Trykk \033[1;32mENTER\033[1;0m for å forsette')
     spacer()
     meny()
 
 def gunstig_mulig():
+    spacer()
+    print('\t\t Gunstige og mulige utfall')
+    spacer()
     print('Når man skal regne ut sannsynligheten for en hendelse, så bruker vi begrepene gunstig og mulig\n'
           '\033[1;32mGunstige utfall\033[1;0m er det vi ønsker skal skje, mens\033[1;32m mulige utfall\033[1;0m er alle hendelser som kan skje.\n'
           'Hvis du kan velge en av fem luker, så vil antall gunstige valg være lik 1 og mulige utfall vil være lik 5\n')
@@ -90,6 +102,9 @@ def gunstig_mulig():
     meny()
 
 def uavhengighet():
+    spacer()
+    print('\t\t Uavhengighet')
+    spacer()
     print('Det finnes situasjoner der vi ønske å vite hvor stor sannsynligheten er for at flere ting skal skje.\n'
           'Dersom situasjonene ikke påvirker hverandre, kaller vi slike hendelser for \033[1;32uavhengige hendelser\033[1;0m.\n'
           'Et eksempel på en uavhengig hendelse vil være å kaste en terning flere ganger. Du vil ikke ha mindre sannsynlighet for å få en sekser på ditt andre kast dersom du fikk sekser på første kast.\n\n'
@@ -112,20 +127,24 @@ def uavhengighet():
 
 def simuleringsprogram():
     def setup():
+        spacer()
+        print('\t\t Simulering')
+        spacer()
         print('Dette er en simulering som viser veivalg.')
         str_1 = str("Elven deler seg i tre. Hvor bred er elv")
-        elv_a = int(input(f"{str_1} A?"))
-        elv_b = int(input(f"{str_1} B?"))
-        elv_c = int(input(f"{str_1} C?"))
-        print('Elv[A] deler seg i to. Hvor bred er elvene?')
-        elv_a_1 = int(input(f"{str_1} A1?"))
-        elv_a_2 = int(input(f"{str_1} A2"))
-        print('Elv[B] deler seg i to. Hvor bred er elvene?')
-        elv_b_1 = int(input(f"{str_1} B1"))
-        elv_b_2 = int(input(f"{str_1} B2"))
-        print('Elv[C] deler seg i to. Hvor bred er elvene?')
-        elv_c_1 = int(input(f"{str_1} C1?"))
-        elv_c_2 = int(input(f"{str_1} C2?"))
+        str_2 = str("deler seg i to. Hvor bred er elv")
+        elv_a = int(input(f"{str_1} A? "))
+        elv_b = int(input(f"{str_1} B? "))
+        elv_c = int(input(f"{str_1} C? "))
+        spacer()
+        elv_a_1 = int(input(f"Elv A {str_2} A1? "))
+        elv_a_2 = int(input(f"Elv A {str_2} A2 "))
+        spacer()
+        elv_b_1 = int(input(f"Elv B {str_2} B1 "))
+        elv_b_2 = int(input(f"Elv B {str_2} B2 "))
+        spacer()
+        elv_c_1 = int(input(f"Elv C {str_2} C1? "))
+        elv_c_2 = int(input(f"Elv C {str_2} C2? "))
 
         return elv_a, elv_b, elv_c, elv_a_1, elv_a_2, elv_b_1, elv_b_2, elv_c_1, elv_c_2
 
@@ -190,34 +209,73 @@ def simuleringsprogram():
 
     elv_a, elv_b, elv_c, elv_a_1, elv_a_2, elv_b_1, elv_b_2, elv_c_1, elv_c_2 = setup()
     teori_sannsyn = teoretisk_sannsynlighet()
-
+    utløp1, utløp2, utløp3, utløp4, utløp5, utløp6 = teori_sannsyn['Utløp 1'],teori_sannsyn['Utløp 2'],teori_sannsyn['Utløp 3'],teori_sannsyn['Utløp 4'],teori_sannsyn['Utløp 5'],teori_sannsyn['Utløp 6'],
     while brukersvar == 'j' or brukersvar == 'J':
 
-        print(teori_sannsyn) # teoretisk sannsynlighet bør pyntes på
-
-        antall_sim = int(input('Hvor mange simuleringer vil du kjøre?'))
+        print('Teoretisk sannsynlighet:     \n' +
+              '                          |                \n' +
+              '                          |                \n' +
+              '                __________|__________           \n' +
+              '               /          |          \          \n' +
+              '             A/          B|           \C        \n' +
+              '             /            |            \        \n' +
+              '      ______/             |             \______ \n' +
+              '      |                   |                    | \n' +
+              '     / \                 / \                  / \ \n' +
+              '  A1/   \A2           B1/   \B2            C1/   \C2 \n' +
+              '   /     \             /     \              /     \ \n' +
+              f'\033[1;32m{utløp1}  {utløp2}      {utløp3}  {utløp4}      {utløp5}  {utløp6}\033[1;0m')
+        spacer()
+        antall_sim = int(input('Hvor mange gjenstander vil du kjøre i simuleringen?'))
 
         oversikt=databehandling(antall_sim)
 
         total = sum(oversikt.values()) / 2
         spacer()
 
-        print('\033[1;32mKg søppel:' + '\t\t\t' + 'prosentdel:\033[1;0m')
-        print('Utløp 1: ' + str(oversikt['elv_a_1']) + '\t\t' + str(format(oversikt['elv_a_1']/total, '.2%')) + '\n' +
-            'Utløp 2: ' + str(oversikt['elv_a_2']) + '\t\t' + str(format(oversikt['elv_a_2']/total, '.2%')) + '\n' +
-            'Utløp 3: ' + str(oversikt['elv_b_1']) + '\t\t' + str(format(oversikt['elv_b_1']/total, '.2%')) + '\n' +
-            'Utløp 4: ' + str(oversikt['elv_b_2']) + '\t\t' + str(format(oversikt['elv_b_2']/total, '.2%')) + '\n' +
-            'Utløp 5: ' + str(oversikt['elv_c_1']) + '\t\t' + str(format(oversikt['elv_c_1']/total, '.2%')) + '\n' +
-            'Utløp 6: ' + str(oversikt['elv_c_2']) + '\t\t' + str(format(oversikt['elv_c_2']/total, '.2%')) + '\n'
-        )
+        print(('\033[1;32mKg søppel:' + '\t' + 'prosentdel:\033[1;0m').expandtabs(27))
+        print(('Utløp 1: ' + str(oversikt['elv_a_1']) + '\t' + str(format(oversikt['elv_a_1']/total, '.2%')) + '\n' +
+            'Utløp 2: ' + str(oversikt['elv_a_2']) + '\t' + str(format(oversikt['elv_a_2']/total, '.2%')) + '\n' +
+            'Utløp 3: ' + str(oversikt['elv_b_1']) + '\t' + str(format(oversikt['elv_b_1']/total, '.2%')) + '\n' +
+            'Utløp 4: ' + str(oversikt['elv_b_2']) + '\t' + str(format(oversikt['elv_b_2']/total, '.2%')) + '\n' +
+            'Utløp 5: ' + str(oversikt['elv_c_1']) + '\t' + str(format(oversikt['elv_c_1']/total, '.2%')) + '\n' +
+            'Utløp 6: ' + str(oversikt['elv_c_2']) + '\t' + str(format(oversikt['elv_c_2']/total, '.2%')) + '\n'
+        ).expandtabs(20))
         print(f' Totalt har det kommet {total} kilo søppel i elva.')
+        sim_utløp1, sim_utløp2, sim_utløp3, sim_utløp4, sim_utløp5, sim_utløp6 = \
+            str(format(oversikt['elv_a_1']/total, '.2%')),\
+            str(format(oversikt['elv_a_2']/total, '.2%')),\
+            str(format(oversikt['elv_b_1']/total, '.2%')),\
+            str(format(oversikt['elv_b_2']/total, '.2%')),\
+            str(format(oversikt['elv_c_1']/total, '.2%')),\
+            str(format(oversikt['elv_c_2']/total, '.2%'))
+
+        print('Simulert sannsynlighet:     \n' +
+              '                          |                \n' +
+              '                          |                \n' +
+              '                __________|__________           \n' +
+              '               /          |          \          \n' +
+              '             A/          B|           \C        \n' +
+              '             /            |            \        \n' +
+              '      ______/             |             \______ \n' +
+              '      |                   |                    | \n' +
+              '     / \                 / \                  / \ \n' +
+              '  A1/   \A2           B1/   \B2            C1/   \C2 \n' +
+              '   /     \             /     \              /     \ \n' +
+              f'\033[1;32m{utløp1}  {utløp2}      {utløp3}  {utløp4}      {utløp5}  {utløp6}\033[1;0m\n' +
+              f'\033[1;31m{sim_utløp1}  {sim_utløp2}      {sim_utløp3}  {sim_utløp4}      {sim_utløp5}  {sim_utløp6}\033[1;0m\n' +
+              '\n\033[1;32mGrønn\033[1;0m teoretisk sannsynlighet \n \033[1;31mRød\033[1;0m  simulert sannsynlighet')
 
         brukersvar=input('Vil du kjøre en ny simulering med samme data? (j/n) ')
 
-    print(teori_sannsyn)
-    print('store talls lov') # fiks bedre tekst
+    print('\n\033[1;32m«De store talls lov»\033[1;0m er et begrep som beskriver det forhold at jo flere tilfeller man har av en hendelse '
+          'jo nærmere vil man komme det forventede resultatet. Hvis man f.eks. kaster seks ganger med en terning kan '
+          'man teoretisk forvente å få en sekser én gang, men i praksis vil det være litt tilfeldig om man får ingen, '
+          'én eller flere seksere. Hvis man derimot kaster 100 ganger med terningen kan man være sikrere på at '
+          'resultatet vil nærme seg at man får en sekser en sjettedel av gangene, og kaster man tusen ganger vil '
+          'man ventelig komme enda nærmere en sjettedel seksere, osv.\n')
 
-    input('Trykk \033[1;32mENTER\033[1;0m for å gå til menyen')
+    input('Trykk \033[1;32mENTER\033[1;0m for å gå til menyen')      #\033[1;32m    \033[1;0m
     spacer()
     meny()
 
@@ -227,4 +285,5 @@ main()
 # format print
 # linjeskift spacing
 # teoretisk sannsynlighet , store talls lov - evt. kommentarsetninger på avvik fra flytskjema
+
 #test daniel
